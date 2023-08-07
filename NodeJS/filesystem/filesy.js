34105fad9c8d632ac8a2c91f04cea0f3cedf7886
Uday Varshney <uv.uday.varshney@gmail.com>
2023-08-07 20:07:12 +0530

    node js

--- NodeJS/filesystem/filesy.js
+++ NodeJS/filesystem/filesy.js
@@ -1,3 +1,15 @@
 const fs = require("fs");
 // This will create new file in the system.
-fs.writeFileSync("newfile.txt", "new file data writeen in the txt doc.");
+// fs.writeFileSync("newfile.txt", "new file data writeen in the txt doc.");
+fs.writeFileSync("newfile.txt", "Overwritten data in this.");
+
+fs.appendFileSync("newfile.txt", " append on the rigth of the file .");
+
+const buf_data = fs.readFileSync("newfile.txt");
+// console.log(buf_data);
+console.log(buf_data.toString());
+// this is another way to convert bufer data into the other format.
+console.log(fs.readFileSync("newfile.txt", "utf8"));
+
+// to rename the file
+// fs.renameSync("newfile.txt", "rename.txt");
