30d9f8b148c9e435c9f46593471c7e2c641360c1
Uday-Varshney <85391692+Uday-Varshney@users.noreply.github.com>
2023-10-14 03:08:09 +0530

    new js

---
+++ JS/From Scrach/operations.js
@@ -0,0 +1,23 @@
+// console.log( 1 + "2"); this prints 12
+// console.log( "1" + 2); this prints 12
+// console.log( "1" + "2"); this prints 12
+// console.log( "1" + 2 + 2); this prints 122
+// console.log( 1 + 2 + "2"); this prints 32
+
+// if string gets first then conversion in string and if the number is first then the conversion in the nimber
+
+// console.log(+true); this prints 1
+// console.log(+""); this prints 0
+
+// comparision
+// console.log(null > 0); false
+// console.log(null == 0); false
+// console.log(null >=0); true
+
+// console.log(undefined > 0); false
+// console.log(undefined == 0); false
+// console.log(undefined >=0); true
+
+// jiss tarike se data store kiya jata aur access kiya  jata h uss basis p do types h
+// primitive:= bigint number string null undefine symbol boolean (callbyvalue).
+// nonprimitive (reference):= arrray objects functions
